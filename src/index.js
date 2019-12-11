3d2c9975865b2107b8260abac0fcb9f6e8526ffb
Eric Wahome <eric.wahome@tingle.software>
2019-12-11 17:05:31 +0300

    updated router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
-
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 const CRouter = ({ routes: appRoutes }) => {
   const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
     <Route
@@ -12,14 +11,20 @@ const CRouter = ({ routes: appRoutes }) => {
       )}
     />
   );
-  return appRoutes.map(route => (
-    <AppRoute
-      exact={route.exact}
-      key={route.comp}
-      path={route.path ? route.path : null}
-      component={route.comp}
-      layout={route.layout}
-    />
-  ));
+  return (
+    <BrowserRouter>
+      <Switch>
+        {appRoutes.map(route => (
+          <AppRoute
+            exact={route.exact}
+            key={route.comp}
+            path={route.path ? route.path : null}
+            component={route.comp}
+            layout={route.layout}
+          />
+        ))}
+      </Switch>
+    </BrowserRouter>
+  );
 };
 export default CRouter;
