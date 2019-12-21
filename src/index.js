c65e0db9e2a697829ccf61afb7707223064f2de8
Eric Wahome <eric.wahome@tingle.software>
2019-12-21 11:01:12 +0300

    added route authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
-const CRouter = ({ routes: appRoutes }) => {
+const CRouter = ({ routes: appRoutes, auth: requireAuth }) => {
   const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
     <Route
       {...rest}
@@ -19,7 +19,7 @@ const CRouter = ({ routes: appRoutes }) => {
             exact={route.exact}
             key={route.comp}
             path={route.path ? route.path : null}
-            component={route.comp}
+            component={requireAuth(route.comp)}
             layout={route.layout}
           />
         ))}
