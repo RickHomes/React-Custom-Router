19f492fc531c0d4655c16cddcae46f90ea27a888
Eric Wahome <eric.wahome@tingle.software>
2019-12-28 12:37:20 +0300

    added return of routes

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const CRouter = ({ routes: appRoutes, auth: requireAuth }) => {
   return (
     <BrowserRouter>
       <Switch>
-        {appRoutes.map(
-          route =>
+        {appRoutes.map(route => {
+          return (
             route.private === true
               ? requireAuth(
                   <AppRoute
@@ -28,27 +28,26 @@ const CRouter = ({ routes: appRoutes, auth: requireAuth }) => {
                   />
                 )
               : null,
-
-          route.private === false ? (
-            <AppRoute
-              exact={route.exact}
-              key={route.comp}
-              path={route.path ? route.path : null}
-              component={route.comp}
-              layout={route.layout}
-            />
-          ) : null,
-
-          route.notFound === true ? (
-            <AppRoute
-              exact={route.exact}
-              key={route.comp}
-              path={route.path ? route.path : null}
-              component={route.comp}
-              layout={route.layout}
-            />
-          ) : null
-        )}
+            route.private === false ? (
+              <AppRoute
+                exact={route.exact}
+                key={route.comp}
+                path={route.path ? route.path : null}
+                component={route.comp}
+                layout={route.layout}
+              />
+            ) : null,
+            route.notFound === true ? (
+              <AppRoute
+                exact={route.exact}
+                key={route.comp}
+                path={route.path ? route.path : null}
+                component={route.comp}
+                layout={route.layout}
+              />
+            ) : null
+          );
+        })}
       </Switch>
     </BrowserRouter>
   );
