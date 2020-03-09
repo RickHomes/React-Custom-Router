0774fee35babe93abd7c73732a377a5d6db8235c
RickHomes <50575139+RickHomes@users.noreply.github.com>
2020-03-09 12:02:43 +0300

    Update index.js
    reconstructed router

--- src/index.js
+++ src/index.js
@@ -1,51 +1,35 @@
 import React from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
-const CRouter = ({ routes: appRoutes, auth: requireAuth }) => {
-  const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
+const CRouter = ({ routes: appRoutes }) => {
+  const AppRoute = ({
+    component: Component,
+    layout: Layout,
+    layoutProps,
+    ...rest
+  }) => (
     <Route
       {...rest}
       render={props => (
-        <Layout>
-          <Component {...props} />
+        <Layout {...layoutProps}>
+          <Component />
         </Layout>
       )}
     />
   );
   return (
-    <BrowserRouter>
-      <Switch>
-        {appRoutes.map(route =>
-          route.private ? (
-            requireAuth(
-              <AppRoute
-                exact={route.exact}
-                key={route.comp}
-                path={route.path ? route.path : null}
-                component={route.comp}
-                layout={route.layout}
-              />
-            )
-          ) : route.notFound ? (
-            <AppRoute
-              exact={route.exact}
-              key={route.comp}
-              path={route.path ? route.path : null}
-              component={route.comp}
-              layout={route.layout}
-            />
-          ) : route.private === false ? (
-            <AppRoute
-              exact={route.exact}
-              key={route.comp}
-              path={route.path ? route.path : null}
-              component={route.comp}
-              layout={route.layout}
-            />
-          ) : null
-        )}
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      {appRoutes.map(route => (
+        <AppRoute
+          exact={route.exact}
+          key={route.comp}
+          path={route.path ? route.path : null}
+          component={route.comp}
+          layout={route.layout}
+          layoutProps={route.layoutProps}
+        />
+      ))}
+    </Switch>
   );
 };
 export default CRouter;
